38e111143f638e230a88eb0f706a2e786d7358b0
lenore-m0rt <lenore.m0rt@gmail.com>
2024-06-05 19:34:28 -0300

    Cambio en next.config.mjs y desinstalación de patch-package

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
+module.exports = {
+  experimental: {
+    forceSwcTransforms: true,
+  },
+}
+
 const nextConfig = {
   reactStrictMode: true,
+    
 };
 
 export default nextConfig;
